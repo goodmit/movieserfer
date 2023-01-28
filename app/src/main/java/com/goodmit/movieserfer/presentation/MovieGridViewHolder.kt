0b5788f5af1c574a727466dd81e31a30c5a28e50
Dmitry Gusev <dmitriy.gusev@fabros-team.com>
2023-01-28 15:15:01 +0300

    movie holder: some movie data binded to view

--- app/src/main/java/com/goodmit/movieserfer/presentation/MovieGridViewHolder.kt
+++ app/src/main/java/com/goodmit/movieserfer/presentation/MovieGridViewHolder.kt
@@ -1,5 +1,7 @@
 package com.goodmit.movieserfer.presentation
 
+import android.content.Context
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -10,18 +12,35 @@ import com.goodmit.movieserfer.R
 import com.goodmit.movieserfer.data.models.Movies
 import com.goodmit.movieserfer.databinding.MovieViewholderBinding
 
+private const val DATE_FORMAT = "dd MMM yyyy"
+
 class MovieGridViewHolder(private val binding: MovieViewholderBinding) : RecyclerView.ViewHolder(binding.root) {
 
     private val titleTextView: TextView = itemView.findViewById(R.id.movie_title)
+    private val ratingTextView: TextView = itemView.findViewById(R.id.rating_title)
+    private val popularityTextView: TextView = itemView.findViewById(R.id.popularity_title)
+    private val dateTextView: TextView = itemView.findViewById(R.id.release_date_TextView)
     private val posterImageView: ImageView = itemView.findViewById(R.id.poster)
 
     fun bind(movie: Movies.Movie) {
         titleTextView.text = movie.title
+        ("${getResourceString(itemView.context, R.string.movie_rating)}: ${movie.voteAverage}")
+            .also { ratingTextView.text = it }
+        ("${getResourceString(itemView.context, R.string.movie_popularity)}:" +
+                " ${movie.popularity}")
+            .also { popularityTextView.text = it }
+        ("${getResourceString(itemView.context, R.string.movie_release_date)}:" +
+                " ${DateFormat.format(DATE_FORMAT, movie.releaseDate)}")
+            .also { dateTextView.text = it }
         with(movie) {
             Glide.with(binding.poster)
                 .load(poster?.medium)
                 .into(posterImageView)
         }
+    }
+
+    private fun getResourceString(context: Context, resId: Int): String {
+        return context.getString(resId)
     }
 
     companion object {
